24af8ebfd111a699a96fca76afb32b409d96c9aa
jpetre0 <83963665+jpetre0@users.noreply.github.com>
2024-01-13 01:48:41 +0000

    introduces schema initialisation functionality to application code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import asyncio
 import string
 import random
 import dotenv
+import psycopg2
 
 
 dotenv.load_dotenv()
@@ -18,3 +19,32 @@ db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
+
+
+with open("database/schema.sql", "r") as sql_file:
+    init_schema = sql_file.read()
+
+
+try:
+    connection = psycopg2.connect(
+        host=db_host, port=db_port, database=db_name, user=db_user, password=db_password
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute(init_schema)
+
+    connection.commit()
+
+    print("getvid-py database schema initialised successfully.")
+
+
+except (Exception, psycopg2.Error) as error:
+    print(f"error: {error}")
+
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("connection closed.")
