2a5f31910dc632cc3474394f0402a14633b6c7f7
jpetre0 <83963665+jpetre0@users.noreply.github.com>
2024-01-13 01:50:03 +0000

    adds db_port variable to application code

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ dotenv.load_dotenv()
 
 token = os.getenv("TOKEN")
 db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
