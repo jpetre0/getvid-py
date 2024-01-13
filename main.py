1a5a484ec84c093b8b995d684d890840cbc3d40d
jpetre0 <83963665+jpetre0@users.noreply.github.com>
2024-01-13 01:27:58 +0000

    adds main python file

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import re
+import discord
+import requests
+import time
+import asyncio
+import string
+import random
+import dotenv
+
+
+load_dotenv()
+
+
+token = os.getenv("TOKEN")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
